85e5c115a6a57417b41b8f3eeb0694ce73f1e829
Daniel Interiano <dannyboyint@gmail.com>
2022-03-15 00:58:31 -0400

    Added navbar and button componenets with their css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Navbar />
+      <Switch>
+        
+      </Switch>
+      </Router>
     </div>
   );
 }
